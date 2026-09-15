8ff66dee13e924db7d1dc173f7e15a5acbbfd81a
anto_hf <thegreatantonio2@gmail.com>
2026-09-15 17:38:15 +0000

    Started editing project

---
+++ Project/horseRace.cpp
@@ -0,0 +1,23 @@
+// Algorithm is in README.md
+
+#include <iostream>
+#include <string>
+
+std::cons TRACK_LENGTH = 12;
+
+srand(time(NULL));
+coin = rand() % 1;
+
+void advance(int horseNum, int* horses);
+void printLane(int horseNum, int* horses);
+bool isWinner(int horseNum, int* horses);
+
+int main(){
+	bool keepGoing = TRUE;
+	while keepGoing(){
+		if isWinner = FALSE {
+			std::cout << "Press ENTER to continue! ";
+		} // end if statement
+	} // end keepGoing
+	return 0;
+} // end main
