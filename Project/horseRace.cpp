452f30b905896d06dba9bd687946bcf110aadcb5
anto_hf <thegreatantonio2@gmail.com>
2026-09-16 16:49:34 +0000

    Edited project

--- Project/horseRace.cpp
+++ Project/horseRace.cpp
@@ -8,16 +8,40 @@ std::cons TRACK_LENGTH = 12;
 srand(time(NULL));
 coin = rand() % 1;
 
-void advance(int horseNum, int* horses);
-void printLane(int horseNum, int* horses);
-bool isWinner(int horseNum, int* horses);
+void advance(int horseNum, int* horses){
+	int turn = dist(rd);
+	horses[hn] += turn;
+} // end advance
+
+void printLane(int horseNum, int* horses){
+	int horseNum = 0;
+	int horsePosition = 0;
+
+        for (int j = 0; j < TRACK_LENGTH; j++) {
+            if (j == horsePosition[i]) {
+                cout << horseNum[i];
+      } else {
+             	 cout << ".";
+            } // end if loop
+        } // end for loop
+        cout << endl;
+    }
+		 } // end nested for loop
+       } // end for loop	 
+} // end printLane
+bool isWinner(int horseNum, int* horses){
+	std::bool winner = FALSE;
+	
+	return winner(horseNum[i])
+} // end isWinner
 
 int main(){
 	bool keepGoing = TRUE;
 	while keepGoing(){
 		if isWinner = FALSE {
-			std::cout << "Press ENTER to continue! ";
-		} // end if statement
-	} // end keepGoing
+			  for (int i = 0; i < MAX - 1; i++) {
+				  printLane(horseNum[i], *horses);
+	} // end for loop
+	  	
 	return 0;
 } // end main
