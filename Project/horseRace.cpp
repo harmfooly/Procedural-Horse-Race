43f426806997b328e4b1f7b885242f01eb1728ae
anto_hf <thegreatantonio2@gmail.com>
2026-09-17 09:04:39 +0000

    Progress on project

--- Project/horseRace.cpp
+++ Project/horseRace.cpp
@@ -2,46 +2,55 @@
 
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <cstdlib>
 
-std::cons TRACK_LENGTH = 12;
 
-srand(time(NULL));
-coin = rand() % 1;
+const int TRACK_LENGTH = 12;
+const int HORSE_NUM = 5;
+
+
 
 void advance(int horseNum, int* horses){
-	int turn = dist(rd);
-	horses[hn] += turn;
+	int coin = std::rand() % 2;
+	if (coin == 1) {
+		horses[horseNum]++;
+	}
 } // end advance
 
 void printLane(int horseNum, int* horses){
-	int horseNum = 0;
-	int horsePosition = 0;
-
         for (int j = 0; j < TRACK_LENGTH; j++) {
-            if (j == horsePosition[i]) {
-                cout << horseNum[i];
+            if (j == horses[horseNum]) {
+		    std::cout << horseNum;
       } else {
-             	 cout << ".";
-            } // end if loop
-        } // end for loop
-        cout << endl;
-    }
-		 } // end nested for loop
-       } // end for loop	 
-} // end printLane
+	      std::cout << ".";
+            } // end if
+        } // end for
+    } // end printLane
+
 bool isWinner(int horseNum, int* horses){
-	std::bool winner = FALSE;
-	
-	return winner(horseNum[i])
+	bool winner = false;
+	if (horses[horseNum] > TRACK_LENGTH) {
+		winner = true;
+	}
+	return winner;
 } // end isWinner
 
 int main(){
-	bool keepGoing = TRUE;
-	while keepGoing(){
-		if isWinner = FALSE {
-			  for (int i = 0; i < MAX - 1; i++) {
-				  printLane(horseNum[i], *horses);
-	} // end for loop
-	  	
+	bool keepGoing = true;
+	int horses[HORSE_NUM] = {0};
+	int turn;
+
+	while (keepGoing) {
+		std::cout << "\n PRESS ANYTHING TO CONTINUE. " << std::endl;
+                std::cin >> turn;	
+		for (int hn = 0; hn < HORSE_NUM; hn++) {
+			printLane(hn, horses);
+			if (isWinner(hn, horses)) {
+				keepGoing = false;
+				std::cout << "Horse " << hn << " wins!" << std::endl;
+			} // end if
+		} // end for
+	} // end while
 	return 0;
 } // end main
