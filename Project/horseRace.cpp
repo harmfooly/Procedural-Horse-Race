899498a08d571f991a7444e4d49070a1fbde8b33
anto_hf <thegreatantonio2@gmail.com>
2026-09-17 10:22:53 +0000

    Edited horse game to include some notes

--- Project/horseRace.cpp
+++ Project/horseRace.cpp
@@ -10,7 +10,7 @@ const int HORSE_NUM = 5;
 
 
 void advance(int horseNum, int* horses){
-	int coinflip = (rand() % 2);
+	int coinflip = (rand() % 2); // 50/50 chance of advancing a horse
 	if (coinflip == 1) {
 		horses[horseNum]++;
 	}
@@ -19,18 +19,18 @@ void advance(int horseNum, int* horses){
 void printLane(int horseNum, int* horses){
         for (int j = 0; j < TRACK_LENGTH + 1; j++) {
             if (j == horses[horseNum]) {
-		    std::cout << horseNum;
+		    std::cout << horseNum; // replaces . with the horse number 
       } else {
 	      std::cout << ". ";
             } // end if
         } // end for
-std::cout << std::endl;
+std::cout << std::endl; // adds spacing after each lane
     } // end printLane
 
-bool isWinner(int horseNum, int* horses){
+bool isWinner(int horseNum, int* horses){ 
 	bool winner = false;
 	if (horses[horseNum] >=  TRACK_LENGTH) {
-		winner = true;
+		winner = true; // just checks if the position of the horse is 
 	}
 	return winner;
 } // end isWinner
@@ -38,9 +38,8 @@ bool isWinner(int horseNum, int* horses){
 int main(){
 	srand(time(NULL));
 	bool keepGoing = true;
-	int horses[HORSE_NUM] = {0};
+	int horses[HORSE_NUM] = {}; // declares the array of horses with 5 horses total
 	int turns = 0;
-
 	while (keepGoing) {
 		turns++;
                 std::cout << "\n PRESS ENTER TO CONTINUE. " << std::endl;
